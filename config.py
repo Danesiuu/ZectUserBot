8b08dfc09370cdbf9b6b50a6822dad465bb47df6
Danesiuu <danee12siuu@gmail.com>
2025-03-01 03:20:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_HASH = os.getenv("API_HASH")
-API_ID = int(os.getenv("API_ID"))
-MONGO_URI = os.getenv("MONGO_URI")
-PREFIX = os.getenv("PREFIX")
-SESSION = os.getenv("SESSION")
-LOG_CHAT = int(os.getenv("LOG_CHAT"))
+API_HASH = os.getenv("cf8f3b25ce525ebee03fdefb62facb4c")
+API_ID = int(os.getenv("23990748"))
+MONGO_URI = os.getenv("mongodb+srv://buyerdanzz:<db_password>@cluster0.lojtw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+PREFIX = os.getenv(".")
+SESSION = os.getenv("BQFuEdwAtBKdV0Lrh7vAsU8yAsRLd-DnJpAGRGK6y3Fip49BBpcJ3xfKK1bH_bjM5AwEPex-FWwpWxQXz4sMK7tZI263ORJASFfmgM0QMGoH0kcWlKlDaEdLm89HJRD11TlF2W_2n7CYRmth1Ukp4WiSuetLrB4a2zVtspteTjqtsqULSX-ryGBNMLopvfcyk12v0GjswF2aKDX0pPhZMrXIKHxBF5ahU048skERp5PCaj5VEblfnJ1m09Oa1b18PUs8bHXpUbRnXbdTLbGDYVFsFwHuv7WWKhdIfnBXs8Jt2liaFOHz8d6Mg3UQssPSHTi1zFV64tIVq-FJ21LIr5jpRU7oWAAAAAGzlkDAAQ")
+LOG_CHAT = int(os.getenv("-4749949638"))
